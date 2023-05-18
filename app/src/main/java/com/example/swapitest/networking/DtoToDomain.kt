82ee9d74d0aa8420165b58a30108571dd197c569
yanuarash <yanuar.ash@gmail.com>
2023-05-19 00:44:45 +0700

    add film dto

--- app/src/main/java/com/example/swapitest/networking/DtoToDomain.kt
+++ app/src/main/java/com/example/swapitest/networking/DtoToDomain.kt
@@ -1,7 +1,12 @@
 package com.example.swapitest.networking
 
+import com.example.swapitest.models.Film
 import com.example.swapitest.models.People
 
 internal fun People.toDomain(): People {
     return People(count, next, previous, results)
+}
+
+internal fun Film.toDomain(): Film {
+    return Film(title)
 }
