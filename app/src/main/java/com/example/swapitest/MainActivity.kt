1f68347569f9128b2cbf0ec7f859b132514af02c
yanuarash <yanuar.ash@gmail.com>
2023-05-18 14:28:05 +0700

    add MainActivity navigation route

--- app/src/main/java/com/example/swapitest/MainActivity.kt
+++ app/src/main/java/com/example/swapitest/MainActivity.kt
@@ -3,41 +3,35 @@ package com.example.swapitest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.swapitest.navigation.NavigationPages
 import com.example.swapitest.ui.theme.SwapiTestTheme
 
+@ExperimentalMaterialApi
+@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SwapiTestTheme {
+            SwapiTestTheme() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val navHostController = rememberNavController()
+                    NavigationPages(navHostController = navHostController)
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SwapiTestTheme {
-        Greeting("Android")
     }
 }
